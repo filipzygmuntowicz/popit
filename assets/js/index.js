e92f4b51c0816ed47001e10aaed79474db2cdfec
Filip <zygmuntowiczf@gmail.com>
2023-08-20 18:46:34 +0200

    changed some class names

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,5 @@
-const hamburgerIcon = document.querySelector('.hamburger_icon');
-const hamburgerMenu = document.querySelector('.hamburger_menu');
+const hamburgerIcon = document.querySelector('.nav__hamburger__icon');
+const hamburgerMenu = document.querySelector('.nav__hamburger__menu');
 const header = document.querySelector('header')
 hamburgerIcon.addEventListener("click", function(){ 
     hamburgerMenu.classList.toggle("active")
